a030383bb7dcc4ea2b45358d7d3a4801ff7263ca
jeromesamir <jerome.samir2@gmail.com>
2015-05-11 12:29:26 +0000

    Tile.java so far, not completed, just a basic outline as discussed in the stand up today

---
+++ Tile.java
@@ -0,0 +1,16 @@
+
+public class Tile {
+
+	public int occupation = 0;		// 0 - vacant, 1 - player1, 2 - player2
+
+	public Tile() {
+	}
+
+	public int getOccupation() {
+		return occupation;
+	}
+	
+	public void setOccupation(int occupation) {
+		this.occupation = occupation;
+	}
+}

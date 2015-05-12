621d691b50bd532ee66c71c98c051efc62e87fc8
jeromesamir <jerome.samir2@gmail.com>
2015-05-12 05:02:59 +0000

    Game.java edited. I have completed the checkHorizontal, checkVertical, checkDiagonal methods and have tested them by running the program and playing games where one player wins in different ways. As far as I can tell there are no bugs in the logic of those methods. The implementation so far is a little weak as there methods are all static. This may have to be resolved later on with the addition of other classes, however, for now at least the logic is correct. I believe this addresses a few of the stories that we placed in the sprint such as the checking who won, and playing a 2 player game. I have added java doc and comments where necessary however, if there are an ambiguities just let me know.
    Just to summarise, you can now play a 2 player game of connect 4, hopefully with no bugs, and with no display other than in the console

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 public class Game {
 
@@ -7,6 +9,7 @@ public class Game {
 		// Set up the board
 		Tile[][] board = new Tile[6][7];
 		
+		// Initialise all the tile instances
 		for (int i = 0; i < 6; i++) {
 			for (int j = 0; j < 7; j++) {
 				board[i][j] = new Tile();
@@ -26,15 +29,45 @@ public class Game {
 		 * The zeroes in the array are the occupations
 		 */
 		
-        
-        Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		int row = 0;
-		int col = 0;
+		int col = 0; // invalid number
+		
+		int printingRow;
+		int printingCol;
 		int playerIdTurn = 1;
 		while (getWinner(board, row, col) == false) {
+			
+			// Displays the game state
+			printingRow = 0;
+			while (printingRow <= 5) {
+				printingCol = 0;
+				if (printingRow > 0) {
+					System.out.println();
+				}
+				while (printingCol <= 6) {
+					System.out.print(board[printingRow][printingCol].getOccupation() + " ");
+					printingCol++;
+				}
+				printingRow++;
+			}
+			System.out.println("\n");
+			
+			
+			// The reason I used the technique of splitting the next line is to safeguard against
+			// incorrect input such as "1 8 104 93 2" for one players input.
 			System.out.print("Player " + playerIdTurn + ", enter a col number > ");
-			col = sc.nextInt();
+			String i = sc.nextLine();
+			String[] input = i.split(" ");
+			col = Integer.parseInt(input[0]);
+			
+			while (col > 7 || col < 1) {
+				System.out.print("Player " + playerIdTurn + ", enter a col number > ");
+				i = sc.nextLine();
+				input = i.split(" ");
+				col = Integer.parseInt(input[0]);
+			}
 					
 			row = placeTile(board, playerIdTurn, col);
 			col = col - 1;
@@ -46,6 +79,7 @@ public class Game {
 				playerIdTurn = 1;
 			}
 		}
+		
 	}
 	
 	
@@ -96,7 +130,7 @@ public class Game {
 		int sameInARow = 0;
 		int lastPlayer = board[rowOfLastPlaced][colOfLastPlaced].getOccupation();
 
-		for (int col = 0; col < 7; col++) {
+		for (int col = 0; col <= 6; col++) {
 			if (board[rowOfLastPlaced][col].getOccupation() == lastPlayer) {
 				sameInARow++;
 				if (sameInARow == 4) {
@@ -112,41 +146,70 @@ public class Game {
 	
 	
 	
-//	public int checkDiagonals(Tile[][] board, int rowOfLastPlaced, int colOfLastPlaced) {
-//		int sameInARow = 0;
-//		int lastPlayer = board[rowOfLastPlaced][colOfLastPlaced].getOccupation();
-//
-//		// CHECKS THE DIAGONAL GOING FROM BOTTOM LEFT TO TOP RIGHT FIRST
-//		for (int row = ) {
-//			
-//		}
-//		
-//		
-//		for (int row = rowOfLastPlaced; row > 0; row--) {
-//			if (board[row][colOfLastPlaced].getOccupation() == lastPlayer) {
-//				sameInARow++;
-//				if (sameInARow == 4) {
-//					return lastPlayer;
-//				}
-//			} else {
-//				return 0;
-//			}
-//		}
-//		
-//		// CHECKS THE DIAGONAL GOING FROM BOTTOM RIGHT TO TOP LEFT SECOND
-//		for (int row = rowOfLastPlaced; row > 0; row--) {
-//			if (board[row][colOfLastPlaced].getOccupation() == lastPlayer) {
-//				sameInARow++;
-//				if (sameInARow == 4) {
-//					return lastPlayer;
-//				}
-//			} else {
-//				return 0;
-//			}
-//		}
-//		
-//		return 0;
-//	}
+	/**
+	 * A method to be called by checkWinner, checks if any player has four of their counters placed
+	 * one after the other diagonally. Checks both diagonals (bottom left to top right, and top left to bottom right)
+	 *@param board					The board instance
+	 * @param rowOfLastPlaced		The row of the last placed counter
+	 * @param colOfLastPlaced		The column of the last placed counter
+	 * @return						A number corresponding to the player that has four in a row, or 0 if neither
+	 * 								ie. 1 for player1, 2 for player2, 0 for no-one.
+	 */
+	public static int checkDiagonals(Tile[][] board, int rowOfLastPlaced, int colOfLastPlaced) {
+		int sameInARow = 0;
+		int lastPlayer = board[rowOfLastPlaced][colOfLastPlaced].getOccupation();
+		int row = rowOfLastPlaced;
+		int col = colOfLastPlaced;
+		
+		// CHECKS THE DIAGONAL GOING FROM BOTTOM LEFT TO TOP RIGHT
+		// gets to the top right of the diagonal you are in
+		while (col < 6 && row > 0) {
+			row--;
+			col++;
+		}
+		
+		while (col >= 0 && row <= 5) {
+			if (board[row][col].getOccupation() == lastPlayer) {
+				sameInARow++;
+				if (sameInARow == 4) {
+					return lastPlayer;
+				}
+			} else {
+				sameInARow = 0;
+			}
+			
+			row++;
+			col--;
+			
+		}
+		
+		
+		sameInARow = 0;
+		row = rowOfLastPlaced;
+		col = colOfLastPlaced;
+		
+		// CHECKS THE DIAGONAL GOING FROM TOP LEFT TO BOTTOM RIGHT
+		// gets to the top left of the diagonal you are in
+		while (col > 0 && row > 0) {
+			row--;
+			col--;
+		}
+		
+		while (row <= 5 && col <= 6) {
+			if (board[row][col].getOccupation() == lastPlayer) {
+				sameInARow++;
+				if (sameInARow == 4) {
+					return lastPlayer;
+				}
+			} else {
+				sameInARow = 0;
+			}
+			row++;
+			col++;
+		}
+		return 0;
+
+	}
 	
 	
 	
@@ -171,21 +234,37 @@ public class Game {
 			winnerId = checkHorizontal(board, rowOfLastPlaced, colOfLastPlaced);
 		}
 		
-		// Checks the two diagonals last
-//		if (winnerId == 0) {
-//			winnerId = checkDiagonals(board, rowOfLastPlaced, colOfLastPlaced);
-//		}
+		//Checks the two diagonals last
+		if (winnerId == 0) {
+			winnerId = checkDiagonals(board, rowOfLastPlaced, colOfLastPlaced);
+		}
 		
 		
 		
 		// Prints out the winners details if someone won
 		if (winnerId != 0) {
-			System.out.println("PLAYER " + winnerId + " WON!");
+			System.out.println("\nPLAYER " + winnerId + " WON!");
+			// Displays the game state
+			int printingRow = 0;
+			int printingCol;
+			while (printingRow <= 5) {
+				printingCol = 0;
+				if (printingRow > 0) {
+					System.out.println();
+				}
+				while (printingCol <= 6) {
+					System.out.print(board[printingRow][printingCol].getOccupation() + " ");
+					printingCol++;
+				}
+				printingRow++;
+			}
+			System.out.println("\n");
 			return true;
 		}
 		
 		return false;
 	}
+	
 	
 	
 	/**

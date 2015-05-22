8a72d667941cb8b68a739d6fcdc8f9ed8b48b531
Michael Bernardi <mrmbernardi@gmail.com>
2015-05-22 05:00:53 +0000

    Implemented resizing of the screen in the GamePanel class.

--- GamePanel.java
+++ GamePanel.java
@@ -7,8 +7,21 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class GamePanel extends JPanel implements MouseMotionListener,MouseListener,GameEventListener{
+public class GamePanel extends JPanel implements MouseMotionListener,MouseListener,ComponentListener,GameEventListener{
 	private GameInstance gi = null;
+	
+	
+	private static final float _CIRCLE_WIDTH = 50; // padding of circles;
+	private static final float _CIRCLE_PADDING = 10; // padding in pixels between two drawn slots.
+	private static final float _LINE_THICKNESS = 3;
+	private static final float _CIRCLE_SPACE = _CIRCLE_WIDTH + _CIRCLE_PADDING;
+	
+	private static float CIRCLE_WIDTH = 50; // padding of circles;
+	private static float CIRCLE_PADDING = 10; // padding in pixels between two drawn slots.
+	private static float LINE_THICKNESS = 3;
+	private static float CIRCLE_SPACE = CIRCLE_WIDTH + CIRCLE_PADDING;
+	
+	private Dimension initialSize;
 	
 	public void setGameInstance(GameInstance gi) {
 		this.gi = gi;
@@ -25,12 +38,14 @@ public class GamePanel extends JPanel implements MouseMotionListener,MouseListen
 	{
 		setLayout(new BorderLayout());//might be a better way to do this.
 		setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-		setPreferredSize(new Dimension(
-				MainFrame.DEFAULT_COLUMN_NUM * (MainFrame.CIRCLE_WIDTH + MainFrame.CIRCLE_PADDING),
-				MainFrame.DEFAULT_ROW_NUM * (MainFrame.CIRCLE_WIDTH + MainFrame.CIRCLE_PADDING)));	
+		initialSize = new Dimension(
+				(int)(MainFrame.DEFAULT_COLUMN_NUM * (CIRCLE_SPACE)),
+				(int)(MainFrame.DEFAULT_ROW_NUM * (CIRCLE_SPACE) + 1));
+		setPreferredSize(initialSize);	
 		setBackground(new Color(150,150,150));
 		addMouseMotionListener(this);
 		addMouseListener(this);
+		addComponentListener(this);
 		
 		//the next line needs to be looked at if we want proper fonts.
 		this.setFont(this.getFont().deriveFont(Font.PLAIN, 24));
@@ -49,45 +64,48 @@ public class GamePanel extends JPanel implements MouseMotionListener,MouseListen
 	private boolean mouseIsInPanel = false;
 	
 	private int selectedColumn;
+	private int screenLeft;
 	
 	@Override
 	public void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
-		
 		// maybe there's a way to only run the next 3 lines once, as opposed to on every paint call.
 		Graphics2D g2d = (Graphics2D)g;
-		g2d.setStroke(new BasicStroke(MainFrame.LINE_THICKNESS));
+		g2d.setStroke(new BasicStroke(LINE_THICKNESS));
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);//makes things look nice
 		
-		for( int x = 0; x < gi.getBoard().numRow(); x++ )
+		//for some reason calculating this elsewhere results in inconsistencies if its not set properly before every paint call
+		screenLeft = (int) ((getSize().getWidth() - gi.getBoard().numCol()*CIRCLE_SPACE)/2);//better to just do it here
+		
+		for( int y = 0; y < gi.getBoard().numRow(); y++ )
 		{
-			for( int y = 0; y < gi.getBoard().numCol(); y++ )
+			for( int x = 0; x < gi.getBoard().numCol(); x++ )
 			{
-				int xpos = (MainFrame.CIRCLE_PADDING/2) + (MainFrame.CIRCLE_SPACE)  * x;
-				int ypos = (MainFrame.CIRCLE_PADDING/2) + (MainFrame.CIRCLE_SPACE)  * y;
+				int xpos = (int) ((CIRCLE_PADDING/2) + (CIRCLE_SPACE)  * x + screenLeft);
+				int ypos = (int) ((CIRCLE_PADDING/2) + (CIRCLE_SPACE)  * y);
 				
-				Participant oc = gi.getBoard().getTile(x,y).getOccupant();
+				Participant oc = gi.getBoard().getTile(y,x).getOccupant();
 				
 				g.setColor(slotColours[0]);
 				
 				if (oc != null)
 					g.setColor(slotColours[oc.getPid()+1]); // added +1 since pid starts at 0 and colors start at 1
 				
-				g.fillOval(ypos, xpos, MainFrame.CIRCLE_WIDTH, MainFrame.CIRCLE_WIDTH);
+				g.fillOval(xpos, ypos, (int)CIRCLE_WIDTH, (int)CIRCLE_WIDTH);
 				g.setColor(Color.black);
-				g.drawOval(ypos, xpos, MainFrame.CIRCLE_WIDTH, MainFrame.CIRCLE_WIDTH);
+				g.drawOval(xpos, ypos, (int)CIRCLE_WIDTH, (int)CIRCLE_WIDTH);
 			}
 		}
 		if(gi.getWinner() == null)
 		{
-			if(mouseIsInPanel && gi.getBoard().hasEmptySlot()) //draw selection rectangle code
+			if(mouseIsInPanel && gi.getBoard().hasEmptySlot() && selectedColumn != -1) //draw selection rectangle code
 			{
 				if(selectedColumn < gi.getBoard().numCol())
 				{
 					Participant oc = gi.getCurrentParticipant();
 					g.setColor(slotColours[oc.getPid()+1]);
-					g.drawRect(selectedColumn * MainFrame.CIRCLE_SPACE, 0, MainFrame.CIRCLE_SPACE, (int)getSize().getHeight() - 3);
+					g.drawRect((int)(selectedColumn * CIRCLE_SPACE + screenLeft), 0, (int)CIRCLE_SPACE, (int)(gi.getBoard().numRow() * CIRCLE_SPACE));
 				}
 			}
 		}
@@ -120,18 +138,21 @@ public class GamePanel extends JPanel implements MouseMotionListener,MouseListen
 	public void mouseMoved(MouseEvent arg0) {
 		mousex = arg0.getX();
 		mousey = arg0.getY();
-		selectedColumn = mousex /  (MainFrame.CIRCLE_SPACE);
+		selectedColumn = (int)((mousex-screenLeft) /  (CIRCLE_SPACE));
+		if(selectedColumn < 0 || selectedColumn >= gi.getBoard().getNumberOfColumns() || mousex < screenLeft)
+		{
+			selectedColumn = -1;
+		}
 		repaint();
 		// set x and y positions of mouse then repaint.		
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {		
-		if (gi.getWinner() == null && gi.getBoard().hasEmptySlot()) {
-			int x = e.getX() /  (MainFrame.CIRCLE_SPACE); // column number
-			
+		if (gi.getWinner() == null && gi.getBoard().hasEmptySlot() && selectedColumn != -1) {
 			Participant curr = gi.getCurrentParticipant();
-			gi.makeMove(curr.makeMove(x));
+			gi.makeMove(curr.makeMove(selectedColumn));
+			
 //			if (gi.getWinner() != null || !(gi.getBoard().hasEmptySlot())) {
 //				isOver = true;
 //			}
@@ -163,5 +184,41 @@ public class GamePanel extends JPanel implements MouseMotionListener,MouseListen
 	@Override
 	public void onGameEvent(int arg) {
 		repaint();
+	}
+
+	@Override
+	public void componentHidden(ComponentEvent arg0) {
+		// do nothing		
+	}
+
+	@Override
+	public void componentMoved(ComponentEvent arg0) {
+		// do nothing
+		
+	}
+	
+	boolean firstResize = true;
+	@Override
+	public void componentResized(ComponentEvent arg0) {
+		Dimension newSize = getSize();
+		float xratio = (float)newSize.getWidth() / (float)initialSize.getWidth();
+		float yratio = (float)newSize.getHeight() / (float)initialSize.getHeight();
+		float sizeRatio = xratio < yratio ? xratio : yratio; //min of two ratios.
+		
+		CIRCLE_WIDTH = sizeRatio * _CIRCLE_WIDTH; // padding of circles;
+		CIRCLE_PADDING = sizeRatio * _CIRCLE_PADDING; // padding in pixels between two drawn slots.
+		LINE_THICKNESS = sizeRatio * _LINE_THICKNESS;
+		CIRCLE_SPACE = sizeRatio * _CIRCLE_SPACE;
+		if(!firstResize)
+		{
+				repaint();
+		}
+		firstResize = false;
+	}
+
+	@Override
+	public void componentShown(ComponentEvent arg0) {
+		// do nothing
+		
 	}
 }
